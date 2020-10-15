05398803fa5b8d3a87d439bdeeb89d0f326f97d2
Ernest Kim <krazykid@gmail.com>
2020-10-14 20:58:49 -0400

    Removed providers

--- provider.tf
+++ provider.tf
@@ -1,11 +1,11 @@
-provider "google" {
-  region      = "us-east4"
-  request_timeout = "60m"
-}
-
-provider "google-beta" {
-  region      = "us-east4"
-  request_timeout = "60m"
-}
-
-provider "random" {}
+//provider "google" {
+//  region      = "us-east4"
+//  request_timeout = "60m"
+//}
+//
+//provider "google-beta" {
+//  region      = "us-east4"
+//  request_timeout = "60m"
+//}
+//
+//provider "random" {}
